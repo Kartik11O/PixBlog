92adf193489d6e87ef9cfb8e030754878b879f28
Kartik <125755557+Kartik11O@users.noreply.github.com>
2023-08-22 03:40:31 +0530

    Add files via upload

--- JS/Animation.js
+++ JS/Animation.js
@@ -1,13 +1,18 @@
-let observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-         entry.target.classList.add("show")
-        }
-        else{
-            entry.target.classList.remove("show")
-        }
-    })
-    })
-
- const hiddenElements = document.querySelectorAll(".animation")  
- hiddenElements.forEach((el) => observer.observe(el));  
+if(screen.width < 580) { 
+    // do any 480 width stuff here, or simply do nothing
+    // return;
+} else {
+    let observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+             entry.target.classList.add("show")
+            }
+            else{
+                entry.target.classList.remove("show")
+            }
+        })
+        })
+    
+     const hiddenElements = document.querySelectorAll(".animation")  
+     hiddenElements.forEach((el) => observer.observe(el));  
+}
